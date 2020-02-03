e253dfef3b11bad2df73da49da1e5617047d2b00
mibaneza <49458798+mibaneza@users.noreply.github.com>
2020-02-02 20:53:41 -0500

    gaaaaaaa

---
+++ precache-manifest.ab4b2961766b97d3b40a187d4f207db7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae941a36ba7c5557ec6ad86a10d6035c",
+    "url": "/my-great-portfolio/index.html"
+  },
+  {
+    "revision": "8d445ccad116eb06795f",
+    "url": "/my-great-portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2b78e12a5e003a57d3b5",
+    "url": "/my-great-portfolio/static/js/2.e5e19296.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/my-great-portfolio/static/js/2.e5e19296.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d445ccad116eb06795f",
+    "url": "/my-great-portfolio/static/js/main.d5688c75.chunk.js"
+  },
+  {
+    "revision": "67fff3cdd4dea7bf6905",
+    "url": "/my-great-portfolio/static/js/runtime-main.04e55a56.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-great-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
